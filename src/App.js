f3d88b68880801560d5b1b323881de25d58ea8d6
Tucker King <tuckerk23@gmail.com>
2018-04-15 23:19:38 -0600

    final game logic

--- src/App.js
+++ src/App.js
@@ -8,42 +8,67 @@ import Title from "./components/Title";
 class App extends Component {
   state = {
     cardInfo,
-    score: 1,
+    score: 0,
     highScore: 0
   };
 
-  randomizeImages = () => {
-    const cardInfo = this.state.cardInfo.sort(() => {
-      return 0.5 - Math.random();
+  endGame = () => {
+    if(this.state.score > this.state.highScore){
+      this.setState({highScore: this.state.score}, function(){
+        console.log(this.state.highScore)
+      })
+    }
+
+    this.state.cardInfo.forEach(card =>{
+      card.count = 0;
     });
-    this.setState({ cardInfo });
-  };
 
-  addScore = () => {
-    this.setState({ score: this.state.score + 1})
-    console.log(this.state.score);
-  };
+    this.setState({score:0});
+    return true;
+  }
 
-  //   console.log(this.state.score);
-  //   console.log(this.state.clicked);
-  // };
+  // Scoring recieved the id from above 
+  gameLogic = id => {
 
-  endGame = () => {};
+    // Get the descendants of each element in the current set of matched elements, filtered by a selector, jQuery object, or element.
+    this.state.cardInfo.find((obj, inj) => {
 
-  gameLogic = () => {};
+      //if id you clicked on is = to object 
+      if(obj.id === id) {
+        if(cardInfo[inj].count === 0) {
+
+          //the cardInfo count on json is equal to the cardInfo json + 1 (keeping track of each card clicked on)
+          cardInfo[inj].count = cardInfo[inj].count + 1;
+          this.setState({score: this.state.score + 1}, function(){
+            
+            console.log(this.state.score);
+          });
+
+          this.state.cardInfo.sort(() => Math.random() - 0.5)
+          return true;
+
+        } else {
+          //Rendering the score 
+          this.endGame();
+        }
+      }
+    });
+  }
 
   render() {
     return (
       <div>
-        <Title />
+        <Title 
+        score={this.state.score}
+        highScore={this.state.highScore}
+        />
         <Wrapper>
           {this.state.cardInfo.map(card => (
             <ImageCard
-              randomizeImages={this.randomizeImages}
-              addScore={this.addScore}
               id={card.id}
               key={card.id}
               image={card.image}
+              gameLogic={this.gameLogic}
             />
           ))}
         </Wrapper>
--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./Title.css";
 
-const Title = () => (
+const Title = props => (
   <div className="header">
     <h1 className="title">Clicky Game</h1>
+    <h2 className="scores">Score: {props.score} High Score: {props.highScore}</h2>
   </div>
 );
 
